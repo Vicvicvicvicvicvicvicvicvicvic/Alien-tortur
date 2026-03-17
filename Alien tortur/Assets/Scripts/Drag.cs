a7f2fee8fce49bdc3706d3b36c7c72b16dc92885
JustCallMeNothing <131609303+JustCallMeNothing@users.noreply.github.com>
2026-03-17 10:02:20 +0100

    skibidi

--- Alien tortur/Assets/Scripts/Drag.cs
+++ Alien tortur/Assets/Scripts/Drag.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class Drag : MonoBehaviour
 {
+
+
+    GameObject objSelected = null;
+
+    private Vector3 origin;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,51 @@ public class Drag : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckHitObject();
+            
+        }
+        if (Input.GetMouseButton(0) && objSelected != null)
+        {
+            DragObject();
+        }
+        if (Input.GetMouseButtonUp(0) && objSelected != null)
+        {
+            DropObject();
+        }
     }
+
+
+    void CheckHitObject()
+    {
+        RaycastHit2D hit2D = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+
+        if (hit2D.collider != null)
+        {
+            GameObject hitfourd = hit2D.collider.gameObject;
+            origin = hitfourd.transform.position;
+
+            if (hitfourd.tag == "Tool")
+            {
+                objSelected = hit2D.transform.gameObject;
+            }
+            
+        }
+
+
+    }
+
+    void DragObject()
+    {
+        objSelected.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane + 10.0f));
+    }
+
+    void DropObject()
+    {
+        objSelected.transform.position = origin;
+        objSelected = null;
+
+    }
+
 }
